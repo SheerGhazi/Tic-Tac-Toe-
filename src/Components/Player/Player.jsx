6f39224ddda06ecdaff4d982dbcde1cbe7e2f411
SheerGhazi <sher.ghazi2192@gmail.com>
2024-10-01 16:32:26 +0500

    fix: PLayer comp

--- src/Components/Player/Player.jsx
+++ src/Components/Player/Player.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 import { useState } from "react";
 
-const Player = ({name,symbol}) => {
+const Player = ({initialName,symbol}) => {
     const [isEditing, setisEditing] = useState(false)
+    const [playerName, setplayerName] = useState(initialName)
+
+        const handleEditClick = ()=>{
+            setisEditing((prevState)=>!prevState)
+            // setisEditing(!isEditing)
+        };
+
+        const handleChange = (event) =>{
+                // console.log(event);
+                setplayerName(event.target.value);
+
+        }
+
+        let editablePlayerName = <span className="text-lg font-semibold tracking-wider">{playerName}</span>;
+
+        if(isEditing  ){
+            editablePlayerName = <input className="bg-gray-600" type="text" defaultValue={playerName} onChange={handleChange} />
+
+        }
 
 
   return (
@@ -10,11 +29,14 @@ const Player = ({name,symbol}) => {
       {" "}
       <li className="flex justify-around items-center  w-1/2">
         <span className="flex justify-between items-center space-x-6">
-          <span className="text-lg font-semibold tracking-wider">{name}</span>
+          {editablePlayerName}
           <span>{symbol}</span>
         </span>
-        <button className="font-medium text-base text-yellow-400 hover:text-yellow-700 cursor-pointer">
-          Edit
+        <button
+        className="font-medium text-base text-yellow-400 hover:text-yellow-700 cursor-pointer"
+        onClick={handleEditClick}
+        >
+           {isEditing ? "Save":"Edit"}
         </button>
       </li>
     </>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,9 @@ import Tic_Tac from "../assets/game-logo.png"
 
 const Home = () => {
   return (
+
+
+    
     <div className="w-full min-h-screen bg-custom-gradient ">
       <div className=" flex flex-col justify-center items-center py-12">
         <img className="size-24" src={Tic_Tac} alt="" />
@@ -18,8 +21,8 @@ const Home = () => {
       <main>
         <div className="game-container container mx-auto w-1/2 bg-gray-900 text-white p-6 rounded-xl ">
           <ol className="flex justify-between ">
-            <Player name="Player 1" symbol="X" />
-            <Player name="Player 2" symbol="O" />
+            <Player initialName="Player 1" symbol="X" />
+            <Player initialName="Player 2" symbol="O" />
           </ol>
           Game Board
         </div>
