16d4339e67b5775f8485b4eaa3591c6d04c3e5c7
SheerGhazi <sher.ghazi2192@gmail.com>
2024-10-14 23:39:59 +0500

    fix: HomePage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -22,22 +22,17 @@ const Home = () => {
         >
           Tic Tac Toe
         </h1>
-
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem provident a soluta reprehenderit maxime voluptas mollitia, quae, voluptatem doloremque magni repellendus? Inventore exercitationem explicabo necessitatibus distinctio, ratione deleniti fuga? Quaerat?</p>
       </div>
 
       <main>
         <div className="game-container container mx-auto w-1/2 bg-gray-900 text-white p-6 rounded-xl ">
           <ol className="flex justify-between  ">
-            <Player  initialName="Player 1" symbol="X" isActive={activePlayer === 'X'} />
+            <Player   initialName="Player 1" symbol="X" isActive={activePlayer === 'X'} />
             <Player initialName="Player 2" symbol="O"  isActive={activePlayer === 'O'} />
           </ol>
           <GameBoard  onSelectSquare={handleSelectSquare} />
         </div>
-        Log
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui maiores vitae sunt repellat eligendi explicabo harum, impedit, autem labore ipsa repellendus. Corrupti, ullam! Ad, vero veritatis. Nemo cum facilis ab?</p>
-        <p></p>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti sit adipisci quia dolor hic beatae maxime eligendi, tenetur inventore debitis non labore aliquam iste obcaecati praesentium! Laborum sed nisi veritatis.</p>
+       
         
       </main>
     </div>
