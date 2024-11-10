69c1095463f42dc90ca41a0c37986ca13dfd5317
SheerGhazi <sher.ghazi2192@gmail.com>
2024-11-10 11:48:36 +0500

    fix : WINNING_COMBINATION

--- src/Components/GameBoard/GameBoard.jsx
+++ src/Components/GameBoard/GameBoard.jsx
@@ -2,13 +2,9 @@
 
 import React from "react";
 
-const initialBoard = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null],
-];
 
-const GameBoard = ({ onSelectSquare,  turns  } ) => {
+
+const GameBoard = ({ onSelectSquare,  board  } ) => {
   
   // const [gameBoard, setGameBoard] = useState(initialBoard)
 
@@ -22,24 +18,18 @@ const GameBoard = ({ onSelectSquare,  turns  } ) => {
   //   onSelectSquare();
   
 
-  let gameBoard = initialBoard;
-  for(const turn of turns){
-    const { square, player} = turn;
-    const { row,col} =square;
-
-    gameBoard [row] [col] = player;
-  }
+  
 
 
   return (
     <ol className="grid grid-cols-3 gap-4 max-w-xs mx-auto mt-10">
-      {gameBoard.map((row, rowIndex) => (
+      {board.map((row, rowIndex) => (
         <li  key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li   key={colIndex}>
                 {" "}
-                <button className="w-20 h-20 text-3xl my-2 font-bold border-2 border-gray-500 flex items-center justify-center " onClick={ ()=>onSelectSquare(rowIndex,colIndex)} >{playerSymbol}</button>{" "}
+                <button className="w-20 h-20 text-3xl my-2 font-bold border-2 border-gray-500 flex items-center justify-center " onClick={ ()=>onSelectSquare(rowIndex,colIndex)} disabled={playerSymbol !== null} >{playerSymbol}</button>{" "}
               </li>
             ))}
           </ol>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,38 +1,60 @@
-import { useState } from 'react'
-import React from 'react'
-import Player from '../Components/Player/Player'
-import Tic_Tac from "../assets/game-logo.png"
-import GameBoard from '../Components/GameBoard/GameBoard'
-import Log from '../Components/Log/Log'
+import { useState } from "react";
+import React from "react";
+import Player from "../Components/Player/Player";
+import Tic_Tac from "../assets/game-logo.png";
+import GameBoard from "../Components/GameBoard/GameBoard";
+import Log from "../Components/Log/Log";
+import { WINNING_COMBINATIONS } from "../Components/WinningCombination/winning-combination";
 
-function deriveActivePlayer(gameTurns){
-  let currentPlayer = 'X';
-  if (gameTurns.length > 0 &&  gameTurns[0].player === 'X'){
-    currentPlayer = 'O';
+const initialBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+
+
+function deriveActivePlayer(gameTurns) {
+  let currentPlayer = "X";
+  if (gameTurns.length > 0 && gameTurns[0].player === "X") {
+    currentPlayer = "O";
   }
-   return currentPlayer;
+  return currentPlayer;
 }
 
 const Home = () => {
-   const [gameTurns, setGameTurns] =  useState([]);
+  const [gameTurns, setGameTurns] = useState([]);
+  // const [hasWinner, setHasWinner] = useState(false)
   // const [activePlayer, setActivePlayer] = useState('X');
 
   const activePlayer = deriveActivePlayer(gameTurns);
 
+  let gameBoard = initialBoard;
+  for(const turn of gameTurns){
+    const { square, player} = turn;
+    const { row,col} =square;
 
- 
-  function handleSelectSquare(rowIndex,colIndex){
-      // setActivePlayer((curActivePlayer)=> curActivePlayer === 'X' ? 'O' : 'X' )
-      setGameTurns((prevTurns) => {
-        const currentPlayer = deriveActivePlayer(prevTurns);
-
-        const updatedTurns = [
-          {square:{row: rowIndex , col: colIndex}, player: currentPlayer},...prevTurns,
-        ];
-        return updatedTurns;
-      });
+    gameBoard [row] [col] = player;
   }
 
+  for ( const  combination of WINNING_COMBINATIONS ){
+    const firstSquareSymbol
+    const secondSquareSymbol
+    const thirdSquareSymbol
+  }
+
+  function handleSelectSquare(rowIndex, colIndex) {
+    // setActivePlayer((curActivePlayer)=> curActivePlayer === 'X' ? 'O' : 'X' )
+    setGameTurns((prevTurns) => {
+      const currentPlayer = deriveActivePlayer(prevTurns);
+
+      const updatedTurns = [
+        { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
+        ...prevTurns,
+      ];
+      return updatedTurns;
+    });
+  }
 
   return (
     <div className="w-full min-h-screen bg-custom-gradient ">
@@ -49,16 +71,23 @@ const Home = () => {
       <main>
         <div className="game-container container mx-auto w-1/2 bg-gray-900 text-white p-6 rounded-xl ">
           <ol className="flex justify-between  ">
-            <Player   initialName="Player 1" symbol="X" isActive={activePlayer === 'X'} />
-            <Player initialName="Player 2" symbol="O"  isActive={activePlayer === 'O'} />
+            <Player
+              initialName="Player 1"
+              symbol="X"
+              isActive={activePlayer === "X"}
+            />
+            <Player
+              initialName="Player 2"
+              symbol="O"
+              isActive={activePlayer === "O"}
+            />
           </ol>
-          <GameBoard  onSelectSquare={handleSelectSquare} turns={gameTurns} />
+          <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
         </div>
-       <Log turns={gameTurns} />
-        
+        <Log turns={gameTurns} />
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
